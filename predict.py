f9345ec1f755e21199df5b62ce0ac6b434e2b270
BibhuLamichhane <lamichhanebibhu0@gmail.com>
2020-09-18 15:12:48 +0545

    trained a new weight for hand detection

--- predict.py
+++ predict.py
@@ -1,27 +1,35 @@
 import cv2
 import numpy as np
 from fingertip_finder import Fingertips
+from hand_finder import YOLO
 
 fingertips = Fingertips(weights='./weights/Retinanet_Fingertip_Detector.h5')
+hand = YOLO(weights='weights/yolo.h5', threshold=0.8)
 
-image = cv2.imread('4.jpg')
-height, width, _ = (128, 128, 3)
+image = cv2.imread('10.jpg')
+# image = cv2.flip(image, 1)
+tl, br = hand.detect(image=image)
+if tl or br is not None:
+    cropped_image = image[tl[1]:br[1], tl[0]: br[0]]
+    height, width, _ = cropped_image.shape
 
-prob, pos = fingertips.classify(im=image)
-pos = np.mean(pos, 0)
+    prob, pos = fingertips.classify(im=cropped_image)
+    pos = np.mean(pos, 0)
 
-prob = np.asarray([(p >= 0.5) * 1.0 for p in prob])
-for i in range(0, len(pos), 2):
-    pos[i] = pos[i] * width
-    pos[i + 1] = pos[i + 1] * height
+    prob = np.asarray([(p >= 0.5) * 1.0 for p in prob])
+    print(prob)
+    for i in range(0, len(pos), 2):
+        pos[i] = pos[i] * width + tl[0]
+        pos[i + 1] = pos[i + 1] * height + tl[1]
 
-index = 0
-image = cv2.rectangle(image, (0, 0), (128, 128), (235, 26, 158), 2)
-for p in prob:
-    if p > 0.5:
-        image = cv2.circle(image, (int(pos[index]), int(pos[index + 1])), radius=3, color=(255, 0, 0), thickness=-2)
-    index = index + 2
+    index = 0
+    image = cv2.rectangle(image, (tl[0], tl[1]), (br[0], br[1]), (235, 26, 158), 2)
+    for p in prob:
+        if p > 0.5:
+            image = cv2.circle(image, (int(pos[index]), int(pos[index + 1])), radius=12, color=(255, 0, 0), thickness=-2)
+        index = index + 2
 
-cv2.imshow('Unified Gesture & Fingertips Detection', image)
-if cv2.waitKey(0) & 0xff == 27:
-    cv2.destroyAllWindows()
+    # display image
+    cv2.imshow('Unified Gesture & Fingertips Detection', image)
+    if cv2.waitKey(0) & 0xff == 27:
+        cv2.destroyAllWindows()
